3db9f95df82cd7b1f1f46271a7f7a801d2556a5c
Liam McLennan <liam.mclennan@gmail.com>
2012-10-15 10:16:33 +1000

    skeleton

---
+++ SpiderfishInterceptor/SpiderfishInterceptorModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace SpiderfishInterceptor
+{
+    public class SpiderfishInterceptorModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
